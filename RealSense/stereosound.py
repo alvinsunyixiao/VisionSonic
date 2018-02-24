d98f47a0eef948a206ba1193243a6bc83482dfa0
Roger Q <twofs0.5@gmail.com>
2018-02-24 16:08:16 -0600

    minor bug fix

--- RealSense/stereosound.py
+++ RealSense/stereosound.py
@@ -2,15 +2,19 @@
 import os
 import sys
 import wave
+import numpy as np
 from openal.audio import SoundData
 from openal.loaders import load_wav_file
 from openal.audio import SoundSink, SoundSource, SoundListener
 import time
 import math
+import config
 
 #do nothing for now. Should be implemented if sound is distant or distorted
 def normalize(a_vector):
-    return a_vector
+    ret = np.array([a_vector[0], a_vector[1], a_vector[2]])
+    scalar_ = np.sqrt(np.sum(ret ** 2))
+    return ret / scalar_
 
 class stereosound:
     def __init__(self):
@@ -21,10 +25,12 @@ class stereosound:
         self.sources = [SoundSource(position = [0, 0, 1], pitch = 5 - i) for i in range(5)]
         #pitch: 5,4,3,2,1
         self.data = load_wav_file("./hey.wav")
+        '''
         for source in self.sources:
             #we want the sources to be looping
             source.looping = True
-            source.queue(data)
+            source.queue(self.data)
+        '''
         #self.sink.play(source1)
         #self.sink.play(source2)
         self.cutoff = [i * (config.MAX_DISTANCE / len(self.sources)) for i in range(len(self.sources))]
@@ -39,7 +45,10 @@ class stereosound:
         return len(self.cutoff) - 1
 
     def play(self, coord_tuple, avg_dis):
-        use_source = self.sources[self.determine_cutoff(avg_dis)]
+        source_ind = self.determine_cutoff(avg_dis)
+        use_source = self.sources[source_ind]
+        use_source.queue(self.data)
+        coord_tuple = normalize(coord_tuple)
         use_source.position = [coord_tuple[0], coord_tuple[1], coord_tuple[2]]
         self.sink.update()
         self.sink.play(use_source)
